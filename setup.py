62b38e7cd48fa35899c92d7c7f30412063170b54
shrikant9793 <shrinaths2@gmail.com>
2024-03-02 01:16:27 +0530

    created environment and installed pacakes

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name= "Air_quality_index",
+    version="0.0.0",
+    author="shrikant9793",
+    author_email="shrinaths2@gmail.com",
+    packages = setuptools.find_packages(where = "src")
+)
